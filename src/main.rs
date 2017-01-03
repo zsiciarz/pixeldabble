5375efd3f54fdc939f7e64a50399287f72441b76
Zbigniew Siciarz <antyqjon@gmail.com>
2017-01-03 20:28:30 +0100

    Calculate histogram for the red channel.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 extern crate image;
 
-use image::{GenericImage, ImageError};
+use image::{GenericImage, ImageError, Pixel};
 
 fn run(filename: &str) -> Result<(), ImageError> {
     let img = image::open(filename)?;
     let (width, height) = img.dimensions();
     println!("{}: {}x{} px", filename, width, height);
+    let mut red_histogram = vec![0; 256];
+    for (_, _, pixel) in img.pixels() {
+        let (r, _, _, _) = pixel.channels4();
+        red_histogram[r as usize] += 1;
+    }
+    println!("{:?}", red_histogram);
     Ok(())
 }
 
