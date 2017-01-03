9431dde2f00a74ec53de6971cc81b2ecf60c39e2
Zbigniew Siciarz <antyqjon@gmail.com>
2017-01-03 20:25:39 +0100

    Read image dimensions.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn run() -> Result<(), String> {
+extern crate image;
+
+use image::{GenericImage, ImageError};
+
+fn run(filename: &str) -> Result<(), ImageError> {
+    let img = image::open(filename)?;
+    let (width, height) = img.dimensions();
+    println!("{}: {}x{} px", filename, width, height);
     Ok(())
 }
 
 fn main() {
-    if let Err(ref e) = run() {
+    let image_file = std::env::args().nth(1).unwrap_or_else(|| {
+        println!("USAGE: pixeldabble <FILENAME>");
+        std::process::exit(1);
+    });
+    if let Err(ref e) = run(&image_file[..]) {
         println!("error: {}", e);
         std::process::exit(1);
     }
