2f6a50d7f2a924642b2a8a4be52ba2160233975a
Zbigniew Siciarz <zbigniew@siciarz.net>
2017-11-24 22:28:09 +0100

    Use Unicode block characters for histogram plots.
    Closes #2.

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,8 @@ pub fn run(filename: &str) -> Result<()> {
         green_histogram[g as usize] += 1;
         blue_histogram[b as usize] += 1;
     }
-    let max = red_histogram.iter()
+    let max = red_histogram
+        .iter()
         .chain(green_histogram.iter())
         .chain(blue_histogram.iter())
         .max()
@@ -39,22 +40,55 @@ pub fn run(filename: &str) -> Result<()> {
 
 fn plot_histogram(histogram: &[i32], max: i32, color: term::color::Color) -> Result<()> {
     // resample histogram from 256 to 128 bins to fit on the screen
-    let histogram: Vec<i32> =
-        histogram.iter().chunks(2).into_iter().map(|chunk| chunk.sum()).collect();
-    let bars: Vec<i32> = histogram.iter().map(|x| HISTOGRAM_HEIGHT * x / max).collect();
+    let histogram: Vec<i32> = histogram
+        .iter()
+        .chunks(2)
+        .into_iter()
+        .map(|chunk| chunk.sum())
+        .collect();
+    let bars: Vec<f32> = histogram
+        .iter()
+        .map(|x| (HISTOGRAM_HEIGHT * x) as f32 / (max as f32))
+        .collect();
     let mut t = term::stdout().ok_or("failed to access stdout")?;
     t.fg(color).chain_err(|| "failed to set font color")?;
     for row in 0..HISTOGRAM_HEIGHT + 1 {
         let mut line = String::with_capacity(histogram.len());
         for bar in &bars {
-            line.push(if *bar >= (HISTOGRAM_HEIGHT - row) {
-                '_'
-            } else {
-                ' '
-            });
+            line.push(to_character(*bar, row));
         }
         println!("{}", line);
     }
     t.reset().chain_err(|| "failed to reset stdout")?;
     Ok(())
+}
+
+fn to_character(bar: f32, row: i32) -> char {
+    let truncated_bar = bar as i32;
+    let top = HISTOGRAM_HEIGHT - row;
+    if truncated_bar > top {
+        '█'
+    } else if truncated_bar == top {
+        let frac = bar.fract();
+        if frac > 0.875 {
+            '▇'
+        } else if frac > 0.75 {
+            '▆'
+        } else if frac > 0.625 {
+            '▅'
+        } else if frac > 0.5 {
+            '▄'
+        } else if frac > 0.375 {
+            '▃'
+        } else if frac > 0.25 {
+            '▂'
+        } else if frac > 0.125 {
+            '▁'
+        } else {
+            ' '
+        }
+
+    } else {
+        ' '
+    }
 }
